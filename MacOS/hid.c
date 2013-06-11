e583f844789f797dab6677013b5cb93d0c9729a3
andrew <andrew@cs.otago.ac.nz>
2013-06-12 10:44:21 +1200

    Cleaning up

--- MacOS/hid.c
+++ MacOS/hid.c
@@ -5,110 +5,84 @@
 */
 #include <IOKit/hid/IOHIDLib.h>
 
-IOHIDManagerRef hidManager;
-CFMutableArrayRef devArray;
-
-/*
-	CFSETCOPYCALLBACK()
-	-------------------
-*/
-void CFSetCopyCallBack(const void *value, void *context)
-{
-CFArrayAppendValue((CFMutableArrayRef)context, value);
-}
-
-/*
-	IOHIDDEVICE_GETLONGPROPERTY()
-	-----------------------------
-*/
-Boolean IOHIDDevice_GetLongProperty(IOHIDDeviceRef inIOHIDDeviceRef, CFStringRef inKey, long *outValue)
-{
-Boolean result = FALSE;
- 
-if (inIOHIDDeviceRef)
-	{
-	assert(IOHIDDeviceGetTypeID() == CFGetTypeID(inIOHIDDeviceRef));
- 
-	CFTypeRef tCFTypeRef = IOHIDDeviceGetProperty(inIOHIDDeviceRef, inKey);
- 
-	if (tCFTypeRef)
-		if (CFNumberGetTypeID() == CFGetTypeID( tCFTypeRef))
-			result = CFNumberGetValue((CFNumberRef) tCFTypeRef, kCFNumberSInt32Type, outValue);
-	}
-	
-return result;
-}
-
-
 /*
 	MAIN()
 	------
 */
 int main(void)
 {
+IOHIDManagerRef hid_manager;
+CFMutableArrayRef devArray;
 char string_buffer[1024];
+CFIndex number_of_devices;
+CFSetRef device_set;
+const IOHIDDeviceRef **device_array;
+const IOHIDDeviceRef **current;
+CFNumberRef vendor, product;
+long vendor_id, product_id;
+CFStringRef manufacturer, product_name;
 
 /*
 	Get a handle to the HID manager
 */
-hidManager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+hid_manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
 
 /*
-	Enumerate all HID devices.
+	Enumerate all HID devices and count how many we have.
 */
-IOHIDManagerSetDeviceMatching(hidManager, NULL);
-IOHIDManagerOpen(hidManager, kIOHIDOptionsTypeNone);
+IOHIDManagerSetDeviceMatching(hid_manager, NULL);
+IOHIDManagerOpen(hid_manager, kIOHIDOptionsTypeNone);
+device_set = IOHIDManagerCopyDevices(hid_manager);
+number_of_devices = CFSetGetCount(device_set);
 
 /*
-	Get the device list and copy it.
+	Tell the user how many we found
 */
-CFSetRef copyOfDevices = IOHIDManagerCopyDevices(hidManager);
-devArray = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
-CFSetApplyFunction(copyOfDevices, CFSetCopyCallBack, (void *)devArray);
+printf("%lld HID devices found\n", (long long)number_of_devices);
 
 /*
-	Find out how many devices we've got.
+	Get the list into a C++ array
 */
-CFIndex nDev = CFArrayGetCount(devArray);
-printf("%d devices found\n",(int)nDev);
-
-/*
-	Finished with the device list so free it.
-*/
-CFRelease(copyOfDevices);
+device_array = new const IOHIDDeviceRef *[number_of_devices];
+CFSetGetValues (device_set, (const void **)device_array);
 
 /*
 	Iterate the device list
 */
-for (long device = 0; device < nDev; device++)
+for (current = device_array; current < device_array + number_of_devices; current++)
 	{
-	IOHIDDeviceRef hidDev = (IOHIDDeviceRef)CFArrayGetValueAtIndex(devArray, device);
-	if (hidDev != NULL)
+	IOHIDDeviceRef hidDev = (IOHIDDeviceRef)*current;
+
+	vendor_id = product_id = 0;
+
+	if ((vendor = (CFNumberRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDVendorIDKey))) != NULL)
+		CFNumberGetValue(vendor, kCFNumberSInt32Type, &vendor_id);
+	printf("VID:%04lX ", vendor_id);
+
+	if ((product = (CFNumberRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDProductIDKey))) != NULL)
+		CFNumberGetValue((CFNumberRef)product, kCFNumberSInt32Type, &product_id);
+	printf("PID:%04lX ", product_id);
+
+	if ((manufacturer = (CFStringRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDManufacturerKey)))!= NULL)
 		{
-		long vendor_id, product_id;
-
-		vendor_id = product_id = 0;
-		IOHIDDevice_GetLongProperty(hidDev, CFSTR(kIOHIDVendorIDKey), &vendor_id);
-		IOHIDDevice_GetLongProperty(hidDev, CFSTR(kIOHIDProductIDKey), &product_id);
-		printf("VID:%04lX PID:%04lX ", vendor_id, product_id);
-
-
-		CFStringRef manufacturer = (CFStringRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDManufacturerKey));
-		if (manufacturer != NULL)
-			{
-			CFStringGetCString(manufacturer, string_buffer, sizeof(string_buffer), kCFStringEncodingUTF8);
-			printf("%s ", string_buffer);
-			}
-
-		CFStringRef product = (CFStringRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDProductKey));
-		if (product != NULL)
-			{
-			CFStringGetCString(product, string_buffer, sizeof(string_buffer), kCFStringEncodingUTF8);
-			printf("(%s)", string_buffer);
-			}
-
-		puts("");
+		CFStringGetCString(manufacturer, string_buffer, sizeof(string_buffer), kCFStringEncodingUTF8);
+		printf("%s ", string_buffer);
 		}
+
+	if ((product_name = (CFStringRef)IOHIDDeviceGetProperty(hidDev, CFSTR(kIOHIDProductKey))) != NULL)
+		{
+		CFStringGetCString(product_name, string_buffer, sizeof(string_buffer), kCFStringEncodingUTF8);
+		printf("(%s)", string_buffer);
+		}
+
+	puts("");
 	}
+
+/*
+	We're finished with the device set and device list so free them.
+*/
+CFRelease(device_set);
+delete [] device_array;
+
 return 0;
 }
